d793b20b0c7d7ad18eaede0b7fbfb8554a15c8f3
imperio0001 <imperio_000@hotmail.com>
2023-02-02 16:10:46 -0300

    incrementa os métodos getByID, getByTitulo (,post , put e delete na classe PostagemController

--- src/main/java/com/blogPessoalItau/repository/PostagemRepository.java
+++ src/main/java/com/blogPessoalItau/repository/PostagemRepository.java
@@ -1,11 +1,15 @@
 package com.blogPessoalItau.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.blogPessoalItau.model.Postagem;
 
+import java.util.List;
+
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
+    List<Postagem> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo);
 
 }
