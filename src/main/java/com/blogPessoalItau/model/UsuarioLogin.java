0daf4f8456b977745cc8a835f7abc5a6f6dddbd9
imperio0001 <imperio_000@hotmail.com>
2023-03-02 16:13:01 -0300

    feat: adiciona deploy

--- src/main/java/com/blogPessoalItau/model/UsuarioLogin.java
+++ src/main/java/com/blogPessoalItau/model/UsuarioLogin.java
@@ -5,6 +5,7 @@ public class UsuarioLogin {
     private Long id;
     private String nome;
     private String usuario;
+
     private String senha;
     private String foto;
     private String token;
